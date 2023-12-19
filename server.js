9bd8371e21813e1567e156fcb69a96d448badd61
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-19 18:37:32 +0000

    Used native mySQL2 function 'createConnection' to create connection object representing a successful connection to  employees_db database

--- server.js
+++ server.js
@@ -3,3 +3,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+
+// Invoking the mysql2 module's native creativeConnection function 
+const connection = mysql.createConnection({
+    // Function takes object as argument made up of the configuration details necessary to establish connection
+    host: "localhost",
+    // default port for the classic MySQL protocol - can specify your own designated port otherwise
+    port: 3306,
+    // Enter your own mySQL username here if set - otherwise defaults to root
+    user: "root",
+    // Enter your own mySQL password here if you have one set:
+    password: "", 
+    database: "employees_db"
+  });
