14ddc3e19202f2cfd9f9f14d30df5f931a405698
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-19 14:11:52 +0000

    server.js script created. Node modules installed previously imported into file

---
+++ server.js
@@ -0,0 +1,5 @@
+// Importation of necessary Node modules/libraries as listeds in dependencies 
+
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
