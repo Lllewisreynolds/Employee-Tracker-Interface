5094b5d5637a865c77cec7a864bb738a101a7070
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-20 10:41:05 +0000

    Connection object used to connect to database via .connect method. Welcome message generated for console. Functions created to query each of our tables and retrieve information to display via console.table. Will be called within initialising initEmployeeDBPrompt function later

--- server.js
+++ server.js
@@ -16,3 +16,61 @@ const connection = mysql.createConnection({
     password: "", 
     database: "employees_db"
   });
+  
+  /* Previously established connection object used to connect to database
+     via calling of connect method */
+  connection.connect(function(err) {
+    if (err) throw err;
+  /* threadId property logged to console - provides unique ID assigned to the current connection established between application and server
+     Backstop to prevent potential conflict/confusion when multiple users access the database concurrently */
+    console.log("connected as ID " + connection.threadId);
+  // Console cleared prior to displaying welcome message
+    console.clear();
+    console.log(" /\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\");
+    console.log("");
+    console.log("          WELCOME TO THE EMPLOYEE DATABASE          ");
+    console.log("");
+    console.log(" \\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\_/\\");
+    initEmployeeDBPrompt();
+  });
+
+// Functions defined for viewing employees, department and role tables - querying the database and then displaying that retrieved information in the console
+// Will be called within initEmployeeDBPrompt later
+function displayEmployees() {
+    /* Columns aliased for readability & easy user comprehension in console
+    Optional Manager information is left joined on the employees table, where the current employee's managerID is matched with another employee's id
+    CONCAT function used to combine manager first and last name into one new 'Manager' Column*/
+    connection.query("SELECT employees.firstName AS First_Name, employees.lastName AS Last_Name, role.title AS Title, role.salary AS Salary, department.name AS Department, CONCAT(e.firstName, ' ' ,e.lastName) AS Manager FROM employees INNER JOIN role on role.id = employees.roleID INNER JOIN department on department.id = role.departmentID LEFT JOIN employees e on employees.managerID = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.log ("");
+      console.log("\x1b[33m          **  EMPLOYEES LIST  **          \x1b[0m");
+      console.log ("");
+      console.table(res)
+      initEmployeeDBPrompt()
+  })
+}
+
+function displayDepts() {
+    connection.query("SELECT department.id AS ID, department.name AS Department FROM department",
+    function(err, res) {
+      if (err) throw err
+      console.log("")
+      console.log(("\x1b[33m          **  DEPARTMENT LIST  **          \x1b[0m"));
+      console.log("")
+      console.table(res)
+      initEmployeeDBPrompt();
+  })
+}
+
+function displayRoles() {
+    connection.query("SELECT role.id AS Dept_ID, role.title AS Title FROM role",
+    function(err, res) {
+      if (err) throw err
+      console.log("")
+      console.log(("\x1b[33m          **  ROLE LIST  **          \x1b[0m"))
+      console.log("")
+      console.table(res)
+      initEmployeeDBPrompt()
+  })
+}
