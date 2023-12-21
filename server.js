7b6a2645ec782261ba893d74c7b8ff25578ea9b6
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-21 09:38:58 +0000

    final MVP function added - update employee role

--- server.js
+++ server.js
@@ -112,7 +112,6 @@ const connection = mysql.createConnection({
             break;
 
             case "Update Current Employee Role":
-                // As of yet undefined
                 updateExistingEmployeeRole();
                 console.log(
                     `You chose to ${answers.action.toLowerCase()}. Please proceed with the next steps.`
@@ -337,3 +336,48 @@ function departmentChoices() {
 })
 return departmentArray;
 }
+
+// Final helper function - enables user to modify existing employee's job title within the database
+function updateExistingEmployeeRole() {
+    connection.query("SELECT employees.lastName, role.title FROM employees JOIN role ON employees.roleID = role.id;", 
+    (err, res) => {
+            if (err) throw err;
+ 
+            inquirer.prompt([
+                {
+                    name: "lastName",
+                    type: "rawlist",
+                    choices: function () {
+                        var lastName = [];
+                        for (var i = 0; i < res.length; i++) {
+                            lastName.push(res[i].lastName);
+                        }
+                        return lastName;
+                    },
+                    message: "What is the existing employee's last name? ",
+                },
+                {
+                    // prompts for new choices
+                    name: "role",
+                    type: "rawlist",
+                    message: "What is the employee's new job title moving forward? ",
+                    choices: roleChoices()
+                },
+            ]).then(function (answers) {
+                // Updating the employee's role in the database based on their choices
+                var roleId = roleChoices().indexOf(answers.role) + 1;
+                connection.query("UPDATE employees SET WHERE ?",
+                    {
+                        lastName: answers.lastName,
+                        roleID: roleId
+                    },
+                    // Handling the results and prompting for further action
+                    function (err) {
+                        if (err)
+                            throw err;
+                        console.table(answers);
+                        initEmployeeDBPrompt();
+                    });
+            });
+        });
+  }
